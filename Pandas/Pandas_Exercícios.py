1e49f373579cab7fe5cd4725ac5e74019d55bf8e
Johnny <63070273+TJfiles@users.noreply.github.com>
2022-06-14 16:27:27 -0300

    Pandas Exercícios

--- Pandas/Pandas_Exercícios.py
+++ Pandas/Pandas_Exercícios.py
@@ -1,6 +1,7 @@
 # https://www.templodocodigo.com.br/post/exerc%C3%ADcios-python-pandas
 
 import pandas as pd
+import numpy as np
 
 salaries = pd.read_csv('Salaries.csv')
 df = pd.DataFrame(salaries)
@@ -22,7 +23,7 @@ print(df['TotalPay'].max(), '\n')
 print(df[df['EmployeeName'] == 'JOSEPH DRISCOLL']['TotalPay'], '\n')
 
 # Ex6- Qual o nome da pessoa que tem o maior salário de todos?
-print(df[df['TotalPay'] == df['TotalPay'].max()]['EmployeeName'])
+print(df[df['TotalPay'] == df['TotalPay'].max()]['EmployeeName'], '\n')
 
 # Ex7- Qual é o salário médio por ano?
 
@@ -31,3 +32,52 @@ print(df[df['TotalPay'] == df['TotalPay'].max()]['EmployeeName'])
 # Ex9- Existe correlação entre ano e salário?
 
 # Ex10- Como criar um outro dataframe selecionando apenas algumas colunas do dataframe original?
+
+
+# https://www.machinelearningplus.com/python/101-pandas-exercises-python/
+
+# 1. How to import pandas and check the version?
+print(pd.__version__, '\n')
+
+# 2. How to create a series from a list, numpy array and dict?
+mylist = list('abcedfghijklmnopqrstuvwxyz')
+myarr = np.arange(26)
+mydict = dict(zip(mylist, myarr))
+
+s1 = pd.Series(mylist)
+s2 = pd.Series(myarr)
+s3 = pd.Series(mydict)
+
+# 3. How to convert the index of a series into a column of a dataframe?
+# Convert the series ser into a dataframe with its index as another column on the dataframe.
+
+mylist = list('abcedfghijklmnopqrstuvwxyz')
+myarr = np.arange(26)
+mydict = dict(zip(mylist, myarr))
+ser = pd.Series(mydict)
+
+df = ser.to_frame().reset_index()
+print(df.head(), '\n')
+
+# 4. How to combine many series to form a dataframe?
+# Combine ser1 and ser2 to form a dataframe.
+
+ser1 = pd.Series(list('abcedfghijklmnopqrstuvwxyz'))
+ser2 = pd.Series(np.arange(26))
+
+df = pd.DataFrame(ser1, ser2)
+print(df.head(), '\n')
+
+# Solution 1
+df = pd.concat([ser1, ser2], axis=1)
+print(df.head(), '\n')
+# Solution 2
+df = pd.DataFrame({'col1': ser1, 'col2': ser2})
+print(df.head(), '\n')
+
+# 5. How to assign name to the series’ index?
+
+ser = pd.Series(list('abcedfghijklmnopqrstuvwxyz'))
+
+ser.name = 'Alphabets'
+print(ser.head(), '\n')
